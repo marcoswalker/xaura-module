a6a1bcd864e90999027cc198d380d256d69d4fee
= <=>
2022-01-02 09:51:37 -0300

    dadoMarcelo

--- module/diceConfig.js
+++ module/diceConfig.js
@@ -476,6 +476,103 @@ export function diceConfig(dice) {
       system: "Bruxo"
     });
     //
+    // Feiticeiro
+    dice.addSystem({ id: "Feiticeiro", name: "Feiticeiro"}, false);
+    dice.addDicePreset({
+      type: 'd2',
+      labels: [
+        '1',
+        'modules/xaura-module/assets/wizard.png'
+      ],
+      system: "Feiticeiro"
+    });
+    dice.addDicePreset({
+        type: 'd6',
+        labels: [
+          '1',
+          '2',
+          '3',
+          '4',
+          '5',
+          'modules/xaura-module/assets/wizard.png'
+        ],
+        system: "Feiticeiro"
+    });
+    dice.addDicePreset({
+        type: 'd8',
+        labels: [
+          '1',
+          '2',
+          '3',
+          '4',
+          '5',
+          '6',
+          '7',
+          'modules/xaura-module/assets/wizard.png'
+        ],
+        system: "Feiticeiro"
+    });
+    dice.addDicePreset({
+      type: 'd10',
+      labels: [
+        '1',
+        '2',
+        '3',
+        '4',
+        '5',
+        '6',
+        '7',
+        '8',
+        '9',
+        'modules/xaura-module/assets/wizard.png'
+      ],
+      system: "Feiticeiro"
+    });
+    dice.addDicePreset({
+        type: 'd12',
+        labels: [
+          '1',
+          '2',
+          '3',
+          '4',
+          '5',
+          '6',
+          '7',
+          '8',
+          '9',
+          '10',
+          '11',
+          'modules/xaura-module/assets/wizard.png',
+        ],
+        system: "Feiticeiro"
+    });
+    dice.addDicePreset({
+      type: 'd20',
+      labels: [
+        '1',
+        '2',
+        '3',
+        '4',
+        '5',
+        '6',
+        '7',
+        '8',
+        '9',
+        '10',
+        '11',
+        '12',
+        '13',
+        '14',
+        '15',
+        '16',
+        '17',
+        '18',
+        '19',
+        'modules/xaura-module/assets/wizard.png',
+      ],
+      system: "Feiticeiro"
+    });
+    //
     dice.addSystem({ id: "Monge", name: "Monge"}, false);
     dice.addDicePreset({
       type: 'd2',
